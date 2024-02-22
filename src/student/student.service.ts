c545cea80161236d42207953f04511a87401e3be
georgijbudik <georgijbudik@gmail.com>
2024-02-22 01:29:28 +0100

    added student logic

---
+++ src/student/student.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Student } from './student.entity';
+import { CreateStudentInput } from './create-student.input';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class StudentService extends Repository<Student> {
+  constructor(private dataSource: DataSource) {
+    super(Student, dataSource.createEntityManager());
+  }
+
+  async getStudent(id: string): Promise<Student> {
+    return this.findOne({ where: { id } });
+  }
+
+  async getAllStudents(): Promise<Student[]> {
+    return this.find();
+  }
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    return this.save(student);
+  }
+}
